129e12ad11b6c87d1edcf2d1376e79687c0fcb65
Tung Nguyen <tungnc3142@gmail.com>
2021-09-29 22:51:42 +0700

    f

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ from fastapi import FastAPI, Form, UploadFile, File
 app = FastAPI(docs_url="/data_mining", redoc_url=None)
 from database import get_customer
 from typing import Optional
+from fastapi.middleware.cors import CORSMiddleware
+origins = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://localhost:8081",
+    "http://128.199.72.145:8081",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/get_emotions')
 async def _get_emotions(from_date:Optional[str]=None,to_date:Optional[str]=None):
